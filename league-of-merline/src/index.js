34593861ba2dfb966247b418bd82b4f554fa7875
Adrien Colombier <adrien.colombier@epitech.eu>
2020-03-27 18:19:15 +0100

    [ADD] - Create a sign page

--- league-of-merline/src/index.js
+++ league-of-merline/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Sign from './Sign';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Sign />
   </React.StrictMode>,
   document.getElementById('root')
 );
