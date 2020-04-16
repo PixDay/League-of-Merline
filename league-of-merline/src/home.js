f98b362dc72890843a086d41cbb2da51154d34aa
colombie <adrien.colombier@epitech.eu>
2020-04-17 01:47:43 +0200

    [MODIFIED] - Splitting work

--- league-of-merline/src/home.js
+++ league-of-merline/src/home.js
@@ -1,50 +1,15 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
+import Navbar from './components/navbar';
 import Fab from '@material-ui/core/Fab';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import IconClear from '@material-ui/icons/Clear';
 import AddIcon from '@material-ui/icons/Add';
-import sideNav from './drawer'
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
 
 export default function ButtonAppBar() {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Merline
-          </Typography>
-          <IconButton edge="end" color="inherit" aria-lable="menu" href="/">
-            <IconClear/>
-          </IconButton>
-        </Toolbar>
-      </AppBar>
+    <div>
+      <Navbar></Navbar>
       <Fab color="primary" aria-label="add" className="addButton">
-        <AddIcon />
+          <AddIcon />
       </Fab>
-      <sideNav></sideNav>
     </div>
-
   );
 }
