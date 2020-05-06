44cde088dd667dcb44ba19d9c5dafb759566153a
colombie <adrien.colombier@epitech.eu>
2020-05-06 02:28:08 +0200

    [MODIFIED] - Modified the path ref of import since some specifics files have moved

--- league-of-merline/src/index.js
+++ league-of-merline/src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/index.css';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Sign from './Sign';
-import CreateAccount from './create-account';
-import Home from './home';
+import SignIn from './components/sign-in';
+import SignUp from './components/sign-up';
+import Home from './components/home';
 
 ReactDOM.render(
   <Router>
-    <Route exact path="/" component={Sign} />
-    <Route exact path="/create-account" component={CreateAccount}/>
+    <Route exact path="/" component={SignIn} />
+    <Route exact path="/sign-up" component={SignUp}/>
     <Route exact path="/home" component={Home} />
   </Router>,
   document.getElementById('root')
@@ -19,4 +19,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
