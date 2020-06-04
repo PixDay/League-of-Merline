c0ae5c8076f35ffa82ef6a1ebe37969bdd81385c
colombie <adrien.colombier@epitech.eu>
2020-06-04 16:09:27 +0200

    [ADD] - Create a left-side drawer to manage preferences and account.

--- Web/league-of-merline/src/components/navbar.js
+++ Web/league-of-merline/src/components/navbar.js
@@ -4,9 +4,8 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import IconClear from '@material-ui/icons/Clear';
-
+import SideBar from './sidebar'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -27,9 +26,7 @@ const useStyles = makeStyles((theme) => ({
       <div className={classes.root}>
         <AppBar position="static">
           <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-              <MenuIcon />
-            </IconButton>
+            <SideBar></SideBar>
             <Typography variant="h6" className={classes.title}>
               Merline
             </Typography>
