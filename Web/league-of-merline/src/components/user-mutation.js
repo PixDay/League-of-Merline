f9561fb3087b873f7852fcaf2d6a32faa93b5e54
colombie <adrien.colombier@epitech.eu>
2020-06-17 19:44:40 +0200

    [MODIFIED] - Modified the queries / mutations code synthaxe

--- Web/league-of-merline/src/components/user-mutation.js
+++ Web/league-of-merline/src/components/user-mutation.js
@@ -2,22 +2,11 @@ import * as client from './client';
 import gql from "graphql-tag";
 
 export function register(accountName, password) {
-    console.log("REGISTERING");
-    console.log("accountName = " + accountName);
-    console.log("password = " + password);
-
-
     const mutation = gql`
-        mutation(
-            $accountName: String!
-            $password: String!
-        ) 
+        mutation ($accountName: String! $password: String!) 
         {
-            register(
-                accountName:$accountName, password:$password) 
-                {
-                    token 
-                } 
+            register(accountName: $accountName, password: $password) 
+                {token} 
         }
     `;
     
--- Web/league-of-merline/src/components/user-query.js
+++ Web/league-of-merline/src/components/user-query.js
@@ -2,43 +2,14 @@ import * as client from './client';
 import gql from "graphql-tag";
 
 export function login(accountName, password, rememberMe) {
-  client.client.query({query: gql`{
-    login(accountName: "${accountName}" password: "${password}" rememberMe: ${rememberMe})
-    {token user{accountName id}}
-  }
-  `}).then(result => {
-    if (result.data["login"] != null) {
-      console.log("CHANGE L'URL BATARD");
-      console.log(result);
+  const query = gql`
+    query($accountName: String! $password: String! $rememberMe: Boolean) {
+      login(accountName: $accountName, password: $password, rememberMe: $rememberMe)
+      {token user {accountName id}}
     }
+  `;
+  
+  client.client.query({query, variables: {accountName, password, rememberMe}}).then(result => {
+      console.log(result);
   });
-}
-
-export function getUsers() {
-    client.client.query({query: gql`
-    {
-      users(orderBy: accountName_ASC)
-      {id accountName}
-    } 
-    `}).then(result => console.log(result));
-}
-
-export function getUser() {
-    client.client.query({query: gql`{
-        user(where: {accountName: "adriencolombier"})
-        {
-          id
-          accountName
-        }
-      }}))`}).then(result => console.log(result));
-}
-
-export function getUsersConnection() {
-    client.client.query({query: gql`
-    `}).then(result => console.log(result));
-}
-
-export function register() {
-    console.log(client.client.mutate({mutation: gql`
-    `}).then(result => console.log(result)));
 }
