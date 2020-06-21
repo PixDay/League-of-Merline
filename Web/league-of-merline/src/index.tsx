dd718e98f4679290210d786051bd783250fdcf97
colombie <adrien.colombier@epitech.eu>
2020-06-21 22:11:34 +0200

    [MODIFIED] - Index.tsx modified updating Router

--- Web/league-of-merline/src/index.tsx
+++ Web/league-of-merline/src/index.tsx
@@ -5,13 +5,15 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import SignIn from './component/sign-in/sign-in';
 import SignUp from './component/sign-up/sign-up';
-import Board from './component/board/board';
+import Profile from './component/profile/profile';
+import Widget from './component/widgets/widgets';
 
 ReactDOM.render(
   <Router>
       <Route exact path="/" component={SignIn} />
       <Route exact path="/sign-up" component={SignUp} />
-      <Route exact path="/board" component={Board} />
+      <Route exact path="/profile" component={Profile} />
+      <Route exact path="/widgets" component={Widget} />
     </Router>,
   document.getElementById('root')
 );
