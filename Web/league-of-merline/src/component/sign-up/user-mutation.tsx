1bb13e1208920ac54c236a7df0c33194662ae8b8
colombie <adrien.colombier@epitech.eu>
2020-06-29 18:54:27 +0200

    Revert "[MODIFIED] - Redirecting to sign-in page once an account is created"
    This reverts commit d69ccc522dad184ba2e1ff944807b5c33d69da22.

--- Web/league-of-merline/src/component/sign-up/user-mutation.tsx
+++ Web/league-of-merline/src/component/sign-up/user-mutation.tsx
@@ -1,8 +1,5 @@
 import * as client from '../client';
 import gql from "graphql-tag";
-import ReactDOM from 'react-dom';
-import React from 'react';
-import SignIn from '../sign-in/sign-in'
 
 export function register(accountName: string, password: string) {
     const mutation = gql`
@@ -13,8 +10,5 @@ export function register(accountName: string, password: string) {
         }
     `;
     
-    client.connection.mutate({mutation, variables: {accountName, password}}).then(result => {
-        if (result["data"]["register"]["token"] != null)
-            ReactDOM.render(<SignIn />, document.getElementById("root"));
-    });
+    client.connection.mutate({mutation, variables: {accountName, password}});
 }
