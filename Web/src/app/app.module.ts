219dfc094211b0daafd45dc96ed8034fbc634579
Adrien Colombier <33284435+PixDay@users.noreply.github.com>
2020-09-24 19:07:33 +0200

    [SAVE] - Save commit to update Angular version

--- Web/src/app/app.module.ts
+++ Web/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from "@angular/flex-layout";
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 
