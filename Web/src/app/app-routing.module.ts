7d99ace57c9e2728af301e950f23514c8fbdea3c
Adrien Colombier <33284435+PixDay@users.noreply.github.com>
2020-09-24 14:44:57 +0200

    [MODIFIED] - Link component to routing system so it work by default

--- Web/src/app/app-routing.module.ts
+++ Web/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sign-in', component: SignInComponent },
+  { path: '',   redirectTo: '/sign-in', pathMatch: 'full' }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
