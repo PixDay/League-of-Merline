ab38fb95e3795336e76b470419a7000682594b93
Adrien Colombier <33284435+PixDay@users.noreply.github.com>
2020-09-25 03:09:27 +0200

    [MODIFIED] - Change the sign-in page look

--- Web/src/app/app.module.ts
+++ Web/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     FlexLayoutModule,
     MatToolbarModule,
     MatInputModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
