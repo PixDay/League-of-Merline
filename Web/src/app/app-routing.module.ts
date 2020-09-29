d5c4f69db025463f28b46d4ec0dca287521fbf82
Adrien Colombier <Merline@localhost.localdomain>
2020-09-29 23:58:45 +0200

    [MODIFIED] - Create a route for home component

--- Web/src/app/app-routing.module.ts
+++ Web/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 const routes: Routes = [
   { path: 'sign-in', component: SignInComponent },
   { path: 'sign-up', component: SignUpComponent },
+  { path: 'home', component: HomeComponent},
   { path: '',   redirectTo: '/sign-in', pathMatch: 'full' }, // redirect to `first-component`
   //{ path: '**', component: PageNotFoundComponent }  // Wildcard route for a 404 page
 ];
