23ccf617f79d18169040f2c8793ccc3916110319
Adrien Colombier <Merline@localhost.localdomain>
2020-09-29 23:56:34 +0200

    [MODIFIED] - Import home component in the app

--- Web/src/app/app.module.ts
+++ Web/src/app/app.module.ts
@@ -14,12 +14,14 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignInComponent,
     SignUpComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
