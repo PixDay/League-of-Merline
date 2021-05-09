53fe58286989f2e7f8d21fddb4c644bdf9bfde43
PixDay <adrien.colombier@epitech.eu>
2021-05-09 20:29:44 +0200

    [ADD] - Creating the route for the pages

--- Web/src/app/app-routing.module.ts
+++ Web/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '',   redirectTo: '/sign-in', pathMatch: 'full' }, // redirect to 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
