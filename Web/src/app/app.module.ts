1116f82157eac4113bf6d29d52f00db45f3c04dc
Adrien Colombier <adrien.colombier@epitech.eu>
2021-05-30 20:25:03 +0200

    [MODIFY] - Component modal moved out of sign-in

--- Web/src/app/app.module.ts
+++ Web/src/app/app.module.ts
@@ -18,12 +18,12 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { HomeComponent } from './components/home/home.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
-import { ScheduleComponent } from './components/sign-in/modals-components/schedule/schedule.component';
-import { GameComponent } from './components/sign-in/modals-components/game/game.component';
-import { CryptoComponent } from './components/sign-in/modals-components/crypto/crypto.component';
-import { ToDoComponent } from './components/sign-in/modals-components/to-do/to-do.component';
-import { DevComponent } from './components/sign-in/modals-components/dev/dev.component';
-import { ArchivementComponent } from './components/sign-in/modals-components/archivement/archivement.component';
+import { ScheduleComponent } from './components/modals-components/schedule/schedule.component';
+import { GameComponent } from './components/modals-components/game/game.component';
+import { CryptoComponent } from './components/modals-components/crypto/crypto.component';
+import { ToDoComponent } from './components/modals-components/to-do/to-do.component';
+import { DevComponent } from './components/modals-components/dev/dev.component';
+import { ArchivementComponent } from './components/modals-components/archivement/archivement.component';
 
 @NgModule({
   declarations: [
