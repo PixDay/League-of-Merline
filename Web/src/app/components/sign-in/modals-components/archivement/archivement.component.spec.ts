dece1b1a125d546788756381d25c1bbab4961ac3
Adrien Colombier <adrien.colombier@epitech.eu>
2021-05-30 20:03:34 +0200

    [ADD] - Archivement modal component

---
+++ Web/src/app/components/sign-in/modals-components/archivement/archivement.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ArchivementComponent } from './archivement.component';
+
+describe('ArchivementComponent', () => {
+  let component: ArchivementComponent;
+  let fixture: ComponentFixture<ArchivementComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ArchivementComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ArchivementComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
