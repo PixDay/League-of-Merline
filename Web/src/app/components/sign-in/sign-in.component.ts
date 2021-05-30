a7172f047f330605cae798951128f935f54daa17
Adrien Colombier <adrien.colombier@epitech.eu>
2021-05-30 20:20:17 +0200

    [MODIFY] - Implement all modal component for the sign-in / login page

--- Web/src/app/components/sign-in/sign-in.component.ts
+++ Web/src/app/components/sign-in/sign-in.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { HomeComponent } from '../home/home.component';
+import { ScheduleComponent } from './modals-components/schedule/schedule.component';
+
 
 @Component({
   selector: 'app-sign-in',
@@ -14,8 +15,48 @@ export class SignInComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  openDialogAgenda() {
-    const dialogRef = this.dialog.open(HomeComponent);
+  openDialogSchedule() {
+    const dialogRef = this.dialog.open(ScheduleComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  openDialogGame() {
+    const dialogRef = this.dialog.open(ScheduleComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  openDialogCrypto() {
+    const dialogRef = this.dialog.open(ScheduleComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  openDialogTodo() {
+    const dialogRef = this.dialog.open(ScheduleComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  openDialogDev() {
+    const dialogRef = this.dialog.open(ScheduleComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  openDialogArchievement() {
+    const dialogRef = this.dialog.open(ScheduleComponent);
 
     dialogRef.afterClosed().subscribe(result => {
       console.log(`Dialog result: ${result}`);
