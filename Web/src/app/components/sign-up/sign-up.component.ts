1d2c6bfe5732729a68ee013165479011dec2a876
Adrien Colombier <adrien.colombier@epitech.eu>
2021-06-03 02:37:20 +0200

    [ADD] - Link modal for sign-up view

--- Web/src/app/components/sign-up/sign-up.component.ts
+++ Web/src/app/components/sign-up/sign-up.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ArchivementComponent } from '../modals-components/archivement/archivement.component';
+import { CryptoComponent } from '../modals-components/crypto/crypto.component';
+import { DevComponent } from '../modals-components/dev/dev.component';
+import { GameComponent } from '../modals-components/game/game.component';
+import { ScheduleComponent } from '../modals-components/schedule/schedule.component';
+import { ToDoComponent } from '../modals-components/to-do/to-do.component';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +14,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
+  }
+
+  openDialogSchedule() {
+    const dialogRef = this.dialog.open(ScheduleComponent, {
+      width: '400px'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  openDialogGame() {
+    const dialogRef = this.dialog.open(GameComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  openDialogCrypto() {
+    const dialogRef = this.dialog.open(CryptoComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  openDialogTodo() {
+    const dialogRef = this.dialog.open(ToDoComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  openDialogDev() {
+    const dialogRef = this.dialog.open(DevComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  openDialogArchievement() {
+    const dialogRef = this.dialog.open(ArchivementComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
 
 }
